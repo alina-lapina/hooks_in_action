409a050c4e575b3d296e626d23dba215eff64315
kons_lapi <alina.lapina@ssb.no>
2019-08-28 14:42:54 +0200

    Convert to personal project

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../images/SSB_logo.png';
+import logo from '../images/AlinaLapina.jpg';
 import '../css/App.css';
 import WelcomePage from './WelcomePage';
 
@@ -8,8 +8,8 @@ export default function App() {
         <div className="App">
             <header className="App-header">
                 <div className="App-name">
-                    <img src={logo} className="App-logo" alt="SSB logo"/>
-                    Klass uttrekk admin
+                    <img src={logo} className="App-logo" alt="Alina Lapina's avatar"/>
+                    React Hooks in action
                 </div>
             </header>
             <WelcomePage/>
