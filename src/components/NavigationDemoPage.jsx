765cf8503d4149b7e3dba1c52f6fda0a91168e06
alina-lapina <Zaq12wsx->
2019-09-15 20:52:16 +0200

    Navigation component structure

--- src/components/NavigationDemoPage.jsx
+++ src/components/NavigationDemoPage.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../css/pages.css';
 
 export default function NavigationDemoPage() {
 
     return (
-        <div className="page">Navigation</div>
-    )}
+        <div className="page">
+            <h1>Navigation</h1>
+            <Navigation>
+                <Step><p>Step 1</p></Step>
+                <Step><p>Step 2</p></Step>
+                <Step><p>Step 3</p></Step>
+            </Navigation>
+        </div>
+    )
+}
+
+export function Navigation({children}) {
+    const [step, setStep] = useState(0);
+
+    return (<>
+            <p>Steps buttons</p>
+            <div>{children[step]}</div>
+            <p>Previous - next</p>
+        </>
+    )
+}
+
+export const Step = ({children}) => {
+    return (<div className="Step">{children}</div>)
+};
